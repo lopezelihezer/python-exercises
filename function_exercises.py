a20a6e1ff33a5e408ce6269a71ef3314aff758e4
lopezelihezer <lopezelihezer@gmail.com>
2021-07-13 12:07:07 -0500

    Added more exercises

--- function_exercises.py
+++ function_exercises.py
@@ -12,3 +12,92 @@ is_two('2')
 
 # 2. Define a function named is_vowel. 
 # It should return True if the passed string is a vowel, False otherwise.
+def is_vowel(string):
+    vowels = ['a', 'e', 'i', 'o', 'u']
+    return string in vowels
+
+# 3. Define a function named is_consonant. 
+# It should return True if the passed string is a consonant, False otherwise. 
+# Use your is_vowel function to accomplish this.
+def is_consonant(string):
+    return is_vowel(string) == False
+
+# 4. Define a function that accepts a string that is a word. 
+# The function should capitalize the first letter of the word if the word starts with a consonant.
+def cap_first(string):
+    if is_consonant(string[0]) == True:
+        x = string.capitalize()
+    else:
+        x = string
+    return x
+
+# 5. Define a function named calculate_tip. 
+# It should accept a tip percentage (a number between 0 and 1) 
+# and the bill total, and return the amount to tip.
+def calculate_tip(tip, bill):
+    if tip > 0 and tip < 1:
+        total_tip = bill * tip
+        bill_total = bill + total_tip
+    else:
+        print('Tip must be a number between 0 and 1')
+    return bill_total
+
+# 6. Define a function named apply_discount. 
+# It should accept a original price, and a discount percentage, and return the price after the discount is applied.
+def apply_discount(o, d):
+    discounted_price = o - (o * d)
+    return discounted_price
+
+# 7. Define a function named handle_commas. 
+# It should accept a string that is a number that contains commas in it as input, and return a number as output.
+def handle_commas(x):
+    new = x.replace(',', '')
+    return new
+
+# 8. Define a function named get_letter_grade. 
+# It should accept a number and return the letter grade associated with that number (A-F).
+def get_letter_grade(num):
+    A = range(90, 101)
+    B = range(80, 89)
+    C = range(70, 79)
+    D = range(60, 69)
+    F = range(0, 59)
+    if num in A:
+        grade = 'A'
+    elif num in B:
+        grade = 'B'
+    elif num in C:
+        grade = 'C'
+    elif num in D:
+        grade = 'D'
+    elif num in F:
+        grade = 'F'
+    else:
+        print('Not gradable')
+    return print(f'You got a(n) {grade}')
+
+# 9. Define a function named remove_vowels that accepts a string and returns a string with all the vowels removed.
+def remove_vowels(word):
+    vowels = ['a', 'A', 'E', 'e', 'I', 'i', 'O', 'o', 'U', 'u']
+    for v in vowels:
+        word = word.replace(v, "")
+    return print(word)
+    
+# 10. Define a function named normalize_name. 
+# It should accept a string and return a valid python identifier, that is:
+# - anything that is not a valid python identifier should be removed
+# - leading and trailing whitespace should be removed
+# - everything should be lowercase
+# - spaces should be replaced with underscores
+# - for example:
+## -- Name will become name
+## -- First Name will become first_name
+## -- % Completed will become completed
+def normalize_name(name):
+    name = name.replace()
+    name = name.strip()
+    name = name.lower()
+    name = name.replace(' ', '_')
+    return name
+
+
